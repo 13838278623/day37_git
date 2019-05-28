1ddb1095da6e980f832cf0f938ea32ca892f66ec
13838278623 <51064645+13838278623@users.noreply.github.com>
2019-05-28 14:33:44 +0800

    Update Demo.java

--- src/com/Demo.java
+++ src/com/Demo.java
@@ -3,10 +3,10 @@ package com;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("$$$$$$$$$$$$$$$$$$$$");
-        System.out.println("********");
+        //System.out.println("********");
         System.out.println("&&&&&&&&&");
 
         int a = 10;
-        int b = 20;
+        
     }
 }
