ddeef43d04cbfa56c8735442c580a32662b64172
13838278623 <1528255648@qq.com>
2019-05-28 14:32:29 +0800

    new info

--- src/com/Demo.java
+++ src/com/Demo.java
@@ -4,8 +4,9 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("$$$$$$$$$$$$$$$$$$$$");
         System.out.println("********");
-        //System.out.println("&&&&&&&&&");
+        System.out.println("&&&&&&&&&");
 
         int a = 10;
+        int b = 20;
     }
 }
